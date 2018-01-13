6d35230da542b6051cbe519f2744f8b78b8d7f19
priya <priya@priya-PC>
2018-01-13 15:06:23 +0530

    mjhgjgj

---
+++ MyGitProjct/src/GitPackg/SignOut.java
@@ -0,0 +1,12 @@
+package GitPackg;
+
+public class SignOut {
+	public static void main(String[] args) {
+		
+		for(int i=0;i<=5;i++){
+			System.out.println(i);
+		}
+		System.out.println("Logout");
+	}
+
+}
